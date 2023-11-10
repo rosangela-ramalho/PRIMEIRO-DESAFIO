157f34cb92b75129bdcff7c952b0a1b15ab3848d
rosangela-ramalho <rosangelaa_ramalho@yahoo.com.br>
2023-11-10 19:13:34 -0300

    commit

---
+++ index.js
@@ -0,0 +1,28 @@
+let nome = " DragãoX";
+let xp = 7001;
+let nivel = Platina;
+
+While (True) 
+ 
+if ( xp <= 1000) {
+    nivel = "Ferro";
+} else if (xp >= 1001 && xp <= 2000) {
+    nivel = "Bronze";
+} else if (xp >= 2001 && xp <= 5000) {
+    nivel = "Prata";
+} else if (xp >= 5001 && xp <= 7000) {
+    nivel = " Ouro";
+} else if (xp >= 7011 && xp <= 8000) {
+    nivel = " Platina";
+} else if (xp >= 8001 && xp <= 9000) {
+    nivel = " Ascendente";
+} else if ( xp >= 9001 && xp <= 10000) {
+    nivel = " Imortal";
+} else if ( xp >= 10001) {
+    nivel = "Radiante";
+}
+
+If ( nomeHeroi != "DragãoX" && xp !==7001)
+console.log (o Heroi Dragão, está no nivel Platina)
+
+break; 
